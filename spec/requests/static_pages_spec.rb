08b40e44bcd80cde4978241b2f2abd6b1894714d
Cameron Taylor <cam.taylor89@gmail.com>
2013-12-30 19:32:59 -0700

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,47 +2,34 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  subject { page }
+
   describe "Home page" do
+  	before { visit root_path }
 
-    it "should have the content 'Statty App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Statty App')
-    end
-
-    it "should have the right title" do
-		visit '/static_pages/home'
-  		expect(page).to have_title("Statty")
-	end
-
-	it "should not have a custom page title" do
-      visit '/static_pages/home'
-      expect(page).not_to have_title('| Home')
-    end
+    it { should have_content('Statty') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| Home') }
   end
 
   describe "Help page" do
+  	before { visit help_path }
 
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-    end
-
-    it "should have the right title" do
-		visit '/static_pages/help'
-  		expect(page).to have_title("Statty | Help")
-	end
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
   end
 
   describe "About page" do
+  	before { visit about_path }
 
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('About Us')
-    end
+    it { should have_content('About') }
+    it { should have_title(full_title('About Us')) }
+  end
 
-    it "should have the right title" do
-		visit '/static_pages/about'
-  		expect(page).to have_title("Statty | About Us")
-	end
+  describe "Contact Page" do
+  	before { visit contact_path }
+
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
   end
 end
